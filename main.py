0c39a6b64369e5d3201d2bbcf7738a17dec3dec7
Tomas Ryba <rybato@gmail.com>
2015-05-06 10:56:30 +0200

    Pridano par komentaru.

--- main.py
+++ main.py
@@ -6,31 +6,31 @@ import numpy as np
 
 import MarkovRandomField
 
+#- - - - - - - - - - - - - - - - - - -
 
-def run(img, seeds):
-    scale = 0.5
-    alpha = 1
-    beta = 1
-    mrf = MarkovRandomField.MarkovRandomField(img, seeds, alpha=alpha, beta=beta, scale=scale)
-    mrf.run()
+# loading the image
+img = skidat.camera()
+n_rows, n_cols = img.shape
 
-#-------------------------------------------------------------------------
-if __name__ == '__main__':
-    img = skidat.camera()
-    n_rows, n_cols = img.shape
-    seeds_1 = (np.array([90, 252, 394]), np.array([220, 212, 108]))
-    seeds_2 = (np.array([68, 265, 490]), np.array([92, 493, 242]))
+# seed points
+seeds_1 = (np.array([90, 252, 394]), np.array([220, 212, 108]))  # first class
+seeds_2 = (np.array([68, 265, 490]), np.array([92, 493, 242]))  # second class
+seeds = np.zeros((n_rows, n_cols), dtype=np.uint8)
+seeds[seeds_1] = 1
+seeds[seeds_2] = 2
 
-    seeds = np.zeros((n_rows, n_cols), dtype=np.uint8)
-    seeds[seeds_1] = 1
-    seeds[seeds_2] = 2
+# plt.figure()
+# plt.imshow(seeds, interpolation='nearest')
+# plt.show()
 
-    # plt.figure()
-    # plt.imshow(seeds, interpolation='nearest')
-    # plt.show()
+# plt.figure()
+# plt.imshow(img, 'gray')
+# plt.show()
 
-    # plt.figure()
-    # plt.imshow(img, 'gray')
-    # plt.show()
+# run(img, seeds)
 
-    run(img, seeds)
+scale = 0.5  # scaling parameter for resizing the image
+alpha = 1  # parameter for weighting the smoothness term (pairwise potentials)
+beta = 1  # parameter for weighting the data term (unary potentials)
+mrf = MarkovRandomField.MarkovRandomField(img, seeds, alpha=alpha, beta=beta, scale=scale)
+mrf.run()
